15d904e980fc07b8a556ccdc3cf73663fff2ee12
yasinkiziltas <yyasinkiziltas@hotmail.com>
2021-03-30 16:47:19 +0300

    async storage

--- App.js
+++ App.js
@@ -5,18 +5,40 @@ import LoginScreen from './screens/LoginScreen'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const AppStack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <AppStack.Navigator headerMode="none">
-        <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
-        <AppStack.Screen name="Login" component={LoginScreen} />
-      </AppStack.Navigator>
-    </NavigationContainer>
-  );
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+
+  React.useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if (value == null) {
+        AsyncStorage.setItem('alreadyLaunched', 'trrue');
+        setIsFirstLaunch(true)
+      } else {
+        setIsFirstLaunch(false)
+      }
+    })
+  }, [])
+
+  if (setIsFirstLaunch == null) {
+    return null;
+  }
+  else if (isFirstLaunch == true) {
+    return (
+      <NavigationContainer>
+        <AppStack.Navigator headerMode="none">
+          <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
+          <AppStack.Screen name="Login" component={LoginScreen} />
+        </AppStack.Navigator>
+      </NavigationContainer>
+    )
+  }
+  else {
+    return <LoginScreen />
+  }
 }
 
 const styles = StyleSheet.create({
