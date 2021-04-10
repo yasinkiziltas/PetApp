66ce0310d3fd4667f04dfd80f579d7fddcf8adbe
Yasin Kızıltaş <yasinkiziltas@Yasin-MacBook-Air.local>
2021-04-11 01:22:30 +0300

    Finish contact

--- screens/ContactPerson.js
+++ screens/ContactPerson.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { View, Text, StyleSheet, Dimensions, Image } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity} from 'react-native'
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
-import { COLORS, FONTS, icons, SIZES, GOOG_API_KEY } from '../constants'
+import { COLORS, FONTS, icons, SIZES, GOOGLE_API_KEY } from '../constants'
+import MapViewDirections from 'react-native-maps-directions';
 
 export default function ContactPerson({ route, navigation }) {
+
+    const mapView = React.useRef();
 
     const [restaurant, setRestaurant] = React.useState(null)
     const [streetName, setStreetName] = React.useState("")
@@ -37,6 +40,41 @@ export default function ContactPerson({ route, navigation }) {
         setRegion(mapRegion)
 
     }, [])
+
+    function calculateAngle(coordinates) {
+        let startLat = coordinates[0]["latitude"]
+        let startLng = coordinates[0]["longitude"]
+        let endLat = coordinates[1]["latitude"]
+        let endLng = coordinates[1]["longitude"]
+        let dx = endLat - startLat
+        let dy = endLng - startLng
+
+        return Math.atan2(dy, dx) * 180 / Math.PI
+    }
+
+    function zoomIn() {
+        let newRegion = {
+            latitude: region.latitude,
+            longitude: region.longitude,
+            latitudeDelta: region.latitudeDelta / 2,
+            longitudeDelta: region.longitudeDelta / 2
+        }
+
+        setRegion(newRegion)
+        mapView.current.animateToRegion(newRegion, 200)
+    }
+
+    function zoomOut() {
+        let newRegion = {
+            latitude: region.latitude,
+            longitude: region.longitude,
+            latitudeDelta: region.latitudeDelta * 2,
+            longitudeDelta: region.longitudeDelta * 2
+        }
+
+        setRegion(newRegion)
+        mapView.current.animateToRegion(newRegion, 200)
+    }
 
     function renderMap() {
         const destinationMarker = () => (
@@ -81,7 +119,7 @@ export default function ContactPerson({ route, navigation }) {
                 coordinate={fromLocation}
                 anchor={{ x: 0.5, y: 0.5 }}
                 flat={true}
-            //rotation
+                rotation={angle}
             >
                 <Image
                     source={icons.car}
@@ -96,10 +134,48 @@ export default function ContactPerson({ route, navigation }) {
         return (
             <View>
                 <MapView
+                    ref={mapView}
                     provider={PROVIDER_GOOGLE}
                     initialRegion={region}
                     style={styles.map}
                 >
+                    <MapViewDirections
+                        origin={fromLocation}
+                        destination={toLocation}
+                        apikey={GOOGLE_API_KEY}
+                        strokeWidth={5}
+                        strokeColor={COLORS.primary}
+                        optimizeWaypoints={true}
+                        onReady={result => {
+                            setDuration(result.duration)
+
+                            if(!isReady){
+                                // Fit route into maps
+                                mapView.current.fitToCoordinates(result.coordinates, {
+                                    edgePadding: {
+                                        right: (SIZES.width / 20),
+                                        bottom: (SIZES.height / 4),
+                                        left: (SIZES.width / 20),
+                                        top: SIZES(height / 8)
+                                    }
+                                })
+
+                                let nextLoc = {
+                                    latitude: result.coordinates[0]["latitude"],
+                                    longitude: result.coordinates[0]["longitude"]
+                                }
+
+                                if (result.coordinates.length >= 2) {
+                                    let angle = calculateAngle(result.coordinates)
+                                    setAngle(angle)
+                                }
+
+                                setFromLocation(nextLoc)
+                                setIsReady(true)
+                            }
+
+                        }}
+                     />
                     {destinationMarker()}
                     {carIcon()}
 
@@ -108,9 +184,100 @@ export default function ContactPerson({ route, navigation }) {
         )
     }
 
+    function renderDestinationHeader() {
+        return (
+            <View
+                style={{
+                    position: 'absolute',
+                    top: 50,
+                    left: 0,
+                    right: 0,
+                    height: 50,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        width: SIZES.width * 0.9,
+                        paddingVertical: SIZES.padding,
+                        paddingHorizontal: SIZES.padding * 2,
+                        borderRadius: SIZES.radius,
+                        backgroundColor: COLORS.white
+                    }}
+                >
+                    <Image
+                        source={icons.red_pin}
+                        style={{
+                            width: 30,
+                            height: 30,
+                            marginRight: SIZES.padding
+                        }}
+                    />
+
+                    <View style={{ flex: 1 }}>
+                        <Text style={{ ...FONTS.body3 }}>{streetName}</Text>
+                    </View>
+
+                    <Text style={{ ...FONTS.body3 }}>{Math.ceil(duration)} mins</Text>
+                </View>
+            </View>
+        )
+    }
+
+    function renderButtons() {
+        return (
+            <View
+                style={{
+                    position: 'absolute',
+                    bottom: SIZES.height * 0.35,
+                    right: SIZES.padding * 2,
+                    width: 60,
+                    height: 130,
+                    justifyContent: 'space-between'
+                }}
+            >
+                {/* Zoom In */}
+                <TouchableOpacity
+                    style={{
+                        width: 60,
+                        height: 60,
+                        borderRadius: 30,
+                        backgroundColor: COLORS.white,
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                    onPress={() => zoomIn()}
+                >
+                    <Text style={{ ...FONTS.body1 }}>+</Text>
+                </TouchableOpacity>
+
+                {/* Zoom Out */}
+                <TouchableOpacity
+                    style={{
+                        width: 60,
+                        height: 60,
+                        borderRadius: 30,
+                        backgroundColor: COLORS.white,
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                    onPress={() => zoomOut()}
+                >
+                    <Text style={{ ...FONTS.body1 }}>-</Text>
+                </TouchableOpacity>
+            </View>
+
+        )
+    }
+
     return (
         <View style={styles.container}>
             {renderMap()}
+            {renderDestinationHeader()}
+            {renderButtons()}
         </View>
     )
 }
